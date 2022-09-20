6b7fa808cacc25f3b2a59e877b682cf3d9325d04
Daniel Card <dan.card@xservus.com>
2022-09-20 09:42:28 +0100

    Update generic_harden01.ps1

--- generic_harden01.ps1
+++ generic_harden01.ps1
@@ -1,37 +1,37 @@
-new-Item -Path "Hklm:\\SOFTWARE\Policies\Microsoft\Windows\Installer" -name AlwaysInstallElevated -Value 0 -Force
+new-Item -Path "hklm:\\SOFTWARE\Policies\Microsoft\Windows\Installer" -name AlwaysInstallElevated -Value 0 -Force
 
-new-Item "Hklm:\\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" -name NoDriveTypeAutoRun -Value 255 -Force
+new-Item "hklm:\\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" -name NoDriveTypeAutoRun -Value 255 -Force
 
-new-Item "Hklm:\\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services" -name fAllowToGetHelp -Value 0 -Force
+new-Item "hklm:\\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services" -name fAllowToGetHelp -Value 0 -Force
 
-new-Item "Hklm:\\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" -name NoAutorun -Value 0 -Force
+new-Item "hklm:\\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" -name NoAutorun -Value 0 -Force
 
-new-Item "Hklm:\\SOFTWARE\Policies\Microsoft\Windows\Explorer" -name NoAutoplayfornonVolume -Value 1 -Force
+new-Item "hklm:\\SOFTWARE\Policies\Microsoft\Windows\Explorer" -name NoAutoplayfornonVolume -Value 1 -Force
 
-new-Item "Hklm:\\SOFTWARE\Policies\Microsoft\Windows\Installer" -name AlwaysInstallElevated -Value 0 -Force
+new-Item "hklm:\\SOFTWARE\Policies\Microsoft\Windows\Installer" -name AlwaysInstallElevated -Value 0 -Force
 
-new-Item "Hklm:\\SYSTEM\CurrentControlSet\Control\Lsa" -name RestrictAnonymous -Value 1 -Force
+new-Item "hklm:\\SYSTEM\CurrentControlSet\Control\Lsa" -name RestrictAnonymous -Value 1 -Force
 
-new-Item "Hklm:\\SYSTEM\CurrentControlSet\Services\Tcpip6\Parameters" -name DisableIPSourceRouting -Value 2 -Force
-New-Item "Hklm:\\SOFTWARE\Policies\Microsoft\WindowsFirewall\DomainProfile" -name DisableNotifications -Value 1 -Force
+new-Item "hklm:\\SYSTEM\CurrentControlSet\Services\Tcpip6\Parameters" -name DisableIPSourceRouting -Value 2 -Force
+New-Item "hklm:\\SOFTWARE\Policies\Microsoft\WindowsFirewall\DomainProfile" -name DisableNotifications -Value 1 -Force
 
-New-Item "Hklm:\\SYSTEM\CurrentControlSet\Services\mrxsmb10" -name Start -Value 4
+New-Item "hklm:\\SYSTEM\CurrentControlSet\Services\mrxsmb10" -name Start -Value 4 -Force
 
-New-Item "Hklm:\\SOFTWARE\policies\Microsoft\office\16.0\common\officeupdate" -name enableautomaticupdates -Value 1 -Force
+New-Item "hklm:\\SOFTWARE\policies\Microsoft\office\16.0\common\officeupdate" -name enableautomaticupdates -Value 1 -Force
 
-New-Item "Hklm:\\SYSTEM\CurrentControlSet\Control\Lsa" -name LmCompatibilityLevel -Value 5 -Force
+New-Item "hklm:\\SYSTEM\CurrentControlSet\Control\Lsa" -name LmCompatibilityLevel -Value 5 -Force
 
-New-item "Hklm:\\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -name LocalAccountTokenFilterPolicy -Value 0 -Force
+New-item "hklm:\\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -name LocalAccountTokenFilterPolicy -Value 0 -Force
 
-New-item "Hklm:\\SOFTWARE\Policies\Microsoft\Windows\Network Connections" -name NC_ShowSharedAccessUI -Value 0 -Force
+New-item "hklm:\\SOFTWARE\Policies\Microsoft\Windows\Network Connections" -name NC_ShowSharedAccessUI -Value 0 -Force
 
-New-item "Hklm:\\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" -name SecurityLayer -Value 2 -Force
+New-item "hklm:\\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" -name SecurityLayer -Value 2 -Force
 
-New-item "Hklm:\\SYSTEM\CurrentControlSet\Control\Lsa" -name RestrictAnonymous -Value 1 -Force
+New-item "hklm:\\SYSTEM\CurrentControlSet\Control\Lsa" -name RestrictAnonymous -Value 1 -Force
 
-New-Item "Hklm:\\SOFTWARE\policies\Microsoft\office\16.0\common\officeupdate" -name enableautomaticupdates -Value 1 -Force
+New-Item "hklm:\\SOFTWARE\policies\Microsoft\office\16.0\common\officeupdate" -name enableautomaticupdates -Value 1 -Force
 
-New-item "Hklm:\\SOFTWARE\Policies\Microsoft\Windows\Network Connections" -name NC_ShowSharedAccessUI -Value 1 -Force
+New-item "hklm:\\SOFTWARE\Policies\Microsoft\Windows\Network Connections" -name NC_ShowSharedAccessUI -Value 1 -Force
 
 New-Item "hklm:\\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" -name SecurityLayer -Value 2 -Force
 
@@ -53,14 +53,18 @@ Set-MpPreference -EnableNetworkProtection Enabled -Force
 
 Set-MpPreference -EnableDnsSinkhole $true -Force
 
-Set-MpPreference -SignatureFallbackOrder {InternalDefinitionUpdateServer|MicrosoftUpdateServer|MMPC}
+#Set-MpPreference -SignatureFallbackOrder {InternalDefinitionUpdateServer|MicrosoftUpdateServer|MMPC}
 
 #Set-MpPreference -SignatureDefinitionUpdateFileSharesSource {\\UNC SHARE PATH|\\UNC SHARE PATH}
 
 Set-MpPreference -SignatureScheduleDay Everyday -Force
 
-Set-MpPreference -SignatureScheduleTime 720
+Set-MpPreference -SignatureScheduleTime 720 -Force
 
 Set-MpPreference -AllowNetworkProtectionOnWinServer $true -Force
 
 Set-MpPreference -CheckForSignaturesBeforeRunningScan $true -Force
+
+New-item "hklm:\\Software\Policies\Microsoft\Edge" -name HideFirstRunExperience -Value 1 -Force
+
+New-Item "hklm:\\Software\Policies\Microsoft\Internet Explorer\Main" -Name DisableFirstRunCustomize -Value 1 -Force
