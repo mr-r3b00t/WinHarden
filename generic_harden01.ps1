77f9f95198533f99516e663a18d65c7c04198b41
Daniel Card <dan.card@xservus.com>
2022-09-22 13:53:14 +0100

    Update generic_harden01.ps1
    i should really look at what i'm writing ;)

--- generic_harden01.ps1
+++ generic_harden01.ps1
@@ -1,47 +1,47 @@
-new-Item -Path "hklm:\\SOFTWARE\Policies\Microsoft\Windows\Installer" -name AlwaysInstallElevated -Value 0 -Force
+New-ItemPropertyProperty -Path "hklm:\\SOFTWARE\Policies\Microsoft\Windows\Installer" -name AlwaysInstallElevated -Value 0 -Force
 
-new-Item "hklm:\\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" -name NoDriveTypeAutoRun -Value 255 -Force
+New-ItemPropertyProperty "hklm:\\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" -name NoDriveTypeAutoRun -Value 255 -Force
 
-new-Item "hklm:\\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services" -name fAllowToGetHelp -Value 0 -Force
+New-ItemProperty "hklm:\\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services" -name fAllowToGetHelp -Value 0 -Force
 
-new-Item "hklm:\\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" -name NoAutorun -Value 0 -Force
+New-ItemProperty "hklm:\\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" -name NoAutorun -Value 0 -Force
 
-new-Item "hklm:\\SOFTWARE\Policies\Microsoft\Windows\Explorer" -name NoAutoplayfornonVolume -Value 1 -Force
+New-ItemProperty "hklm:\\SOFTWARE\Policies\Microsoft\Windows\Explorer" -name NoAutoplayfornonVolume -Value 1 -Force
 
-new-Item "hklm:\\SOFTWARE\Policies\Microsoft\Windows\Installer" -name AlwaysInstallElevated -Value 0 -Force
+New-ItemProperty "hklm:\\SOFTWARE\Policies\Microsoft\Windows\Installer" -name AlwaysInstallElevated -Value 0 -Force
 
-new-Item "hklm:\\SYSTEM\CurrentControlSet\Control\Lsa" -name RestrictAnonymous -Value 1 -Force
+New-ItemProperty "hklm:\\SYSTEM\CurrentControlSet\Control\Lsa" -name RestrictAnonymous -Value 1 -Force
 
-new-Item "hklm:\\SYSTEM\CurrentControlSet\Services\Tcpip6\Parameters" -name DisableIPSourceRouting -Value 2 -Force
-New-Item "hklm:\\SOFTWARE\Policies\Microsoft\WindowsFirewall\DomainProfile" -name DisableNotifications -Value 1 -Force
+New-ItemProperty "hklm:\\SYSTEM\CurrentControlSet\Services\Tcpip6\Parameters" -name DisableIPSourceRouting -Value 2 -Force
+New-ItemProperty "hklm:\\SOFTWARE\Policies\Microsoft\WindowsFirewall\DomainProfile" -name DisableNotifications -Value 1 -Force
 
-New-Item "hklm:\\SYSTEM\CurrentControlSet\Services\mrxsmb10" -name Start -Value 4 -Force
+New-ItemProperty "hklm:\\SYSTEM\CurrentControlSet\Services\mrxsmb10" -name Start -Value 4 -Force
 
-New-Item "hklm:\\SOFTWARE\policies\Microsoft\office\16.0\common\officeupdate" -name enableautomaticupdates -Value 1 -Force
+New-ItemProperty "hklm:\\SOFTWARE\policies\Microsoft\office\16.0\common\officeupdate" -name enableautomaticupdates -Value 1 -Force
 
-New-Item "hklm:\\SYSTEM\CurrentControlSet\Control\Lsa" -name LmCompatibilityLevel -Value 5 -Force
+New-ItemProperty "hklm:\\SYSTEM\CurrentControlSet\Control\Lsa" -name LmCompatibilityLevel -Value 5 -Force
 
-New-item "hklm:\\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -name LocalAccountTokenFilterPolicy -Value 0 -Force
+New-ItemProperty "hklm:\\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -name LocalAccountTokenFilterPolicy -Value 0 -Force
 
-New-item "hklm:\\SOFTWARE\Policies\Microsoft\Windows\Network Connections" -name NC_ShowSharedAccessUI -Value 0 -Force
+New-ItemProperty "hklm:\\SOFTWARE\Policies\Microsoft\Windows\Network Connections" -name NC_ShowSharedAccessUI -Value 0 -Force
 
-New-item "hklm:\\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" -name SecurityLayer -Value 2 -Force
+New-ItemProperty "hklm:\\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" -name SecurityLayer -Value 2 -Force
 
-New-item "hklm:\\SYSTEM\CurrentControlSet\Control\Lsa" -name RestrictAnonymous -Value 1 -Force
+New-ItemProperty "hklm:\\SYSTEM\CurrentControlSet\Control\Lsa" -name RestrictAnonymous -Value 1 -Force
 
-New-Item "hklm:\\SOFTWARE\policies\Microsoft\office\16.0\common\officeupdate" -name enableautomaticupdates -Value 1 -Force
+New-ItemProperty "hklm:\\SOFTWARE\policies\Microsoft\office\16.0\common\officeupdate" -name enableautomaticupdates -Value 1 -Force
 
-New-item "hklm:\\SOFTWARE\Policies\Microsoft\Windows\Network Connections" -name NC_ShowSharedAccessUI -Value 1 -Force
+New-ItemProperty "hklm:\\SOFTWARE\Policies\Microsoft\Windows\Network Connections" -name NC_ShowSharedAccessUI -Value 1 -Force
 
-New-Item "hklm:\\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" -name SecurityLayer -Value 2 -Force
+New-ItemProperty "hklm:\\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" -name SecurityLayer -Value 2 -Force
 
-New-item "hklm:\\Software\Policies\Microsoft\Windows Defender\Real-Time Protection" -Name DisableBehaviorMonitoring -Value 1 -Force
+New-ItemProperty "hklm:\\Software\Policies\Microsoft\Windows Defender\Real-Time Protection" -Name DisableBehaviorMonitoring -Value 1 -Force
 
-New-Item "hklm:\\SOFTWARE\Policies\Microsoft\Windows\Network Connections" -name NC_ShowSharedAccessUI -Value 0 -Force
+New-ItemProperty "hklm:\\SOFTWARE\Policies\Microsoft\Windows\Network Connections" -name NC_ShowSharedAccessUI -Value 0 -Force
 
-New-Item "hklm:\\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services" -name UserAuthentication -Value 1 -Force
+New-ItemProperty "hklm:\\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services" -name UserAuthentication -Value 1 -Force
 
-New-item "hklm:\\SYSTEM\CurrentControlSet\Control\Lsa" -name RestrictAnonymous -Value 1 -Force
+New-ItemProperty "hklm:\\SYSTEM\CurrentControlSet\Control\Lsa" -name RestrictAnonymous -Value 1 -Force
 #Remove Windows Mail App
 Get-AppxPackage Microsoft.windowscommunicationsapps -ErrorAction SilentlyContinue | Remove-AppxPackage -ErrorAction SilentlyContinue
 
@@ -65,6 +65,6 @@ Set-MpPreference -AllowNetworkProtectionOnWinServer $true -Force
 
 Set-MpPreference -CheckForSignaturesBeforeRunningScan $true -Force
 
-New-item "hklm:\\Software\Policies\Microsoft\Edge" -name HideFirstRunExperience -Value 1 -Force
+New-ItemProperty "hklm:\\Software\Policies\Microsoft\Edge" -name HideFirstRunExperience -Value 1 -Force
 
-New-Item "hklm:\\Software\Policies\Microsoft\Internet Explorer\Main" -Name DisableFirstRunCustomize -Value 1 -Force
+New-ItemProperty "hklm:\\Software\Policies\Microsoft\Internet Explorer\Main" -Name DisableFirstRunCustomize -Value 1 -Force
